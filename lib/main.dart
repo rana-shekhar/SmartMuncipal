b09a2c0663456d216bef4f8bdd51b08c5ab0f026
rana-shekhar <shekharrana1011@gmail.com>
2025-03-01 12:42:46 +0530

    Integrate Firebase SDK with authentication and Firestore support

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  print("Firebase Connected Successfully");
+
   runApp(MyApp());
 }
 
@@ -10,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Recruiters App',
+      title: 'Login Page',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
